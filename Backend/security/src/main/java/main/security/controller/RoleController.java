d2bed697a6d3d050478fede96979ef7d31bad867
Piotr Spętany <kapj24l-4@sr.dmcs.pl>
2024-12-12 17:34:35 +0100

    quick fix v2

--- Backend/security/src/main/java/main/security/controller/RoleController.java
+++ Backend/security/src/main/java/main/security/controller/RoleController.java
@@ -2,6 +2,7 @@ package main.security.controller;
 
 import main.security.model.UserRole;
 import main.security.service.UserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +11,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/role")
 public class RoleController {
-    UserRoleService userRoleService;
+    private final UserRoleService userRoleService;
 
-
+    @Autowired
     public RoleController(UserRoleService userRoleService) {
         this.userRoleService = userRoleService;
     }
@@ -24,7 +25,8 @@ public class RoleController {
     }
 
     @GetMapping("/delete")
-    public ResponseEntity<String> deleteRole() {
+    public ResponseEntity<String> deleteRole(@RequestParam("role") String role) {
+        userRoleService.deleteUserRole(role);
         return ResponseEntity.ok("Role removed");
     }
 
